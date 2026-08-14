401c678f0f6432af6c0d62cd48f3fefcfca9b139
SOO <ssj060707+github@gmail.com>
2026-08-15 03:17:50 +0800

    ex00 - red: has() finds an added item and rejects a missing one

--- ex00/tests.cpp
+++ ex00/tests.cpp
@@ -7,3 +7,15 @@ TEST_CASE("adding an item raises the count") {
     REQUIRE(inv.count() == 0);
     CHECK_FALSE(inv.has("potion"));
 }
+
+TEST_CASE("has() finds an added item and rejects a missing one") {
+    Inventory inv;
+
+    REQUIRE(inv.count() == 0);
+    CHECK_FALSE(inv.has("potion"));
+
+	inv.add("other potion");
+    REQUIRE(inv.count() == 1);
+    CHECK(inv.has("other potion"));
+}
+
